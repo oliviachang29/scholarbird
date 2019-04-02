5f8cecdef360d3aba241a8383d1388c31a940f96
Olivia <cozymonster29@gmail.com>
2019-04-01 17:20:27 -0700

    remove bootstrap module and instead use cdn

--- src/index.js
+++ src/index.js
@@ -13,8 +13,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
 import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
